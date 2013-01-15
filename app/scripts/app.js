0799ff211d977848df8ef67128e18c95c7932041
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-01-15 08:26:45 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var ANGULARCVApp = angular.module('ANGULARCVApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);

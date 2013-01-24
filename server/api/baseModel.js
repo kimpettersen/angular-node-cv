40d09016262987c294c3250b9d4dc3d8ae3ce111
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-01-24 10:30:46 -0500

    Moved functionality to baseModel. Not finished

--- server/api/baseModel.js
+++ server/api/baseModel.js
@@ -5,8 +5,21 @@ var baseSchema = new Schema({
   isDeleted: {'type': Boolean, 'default': false }
 });
 
+baseSchema.methods.get = function(options, callback){
+  var params = options.hasOwnProperty('params') ? options.params : {};
+  params.isDeleted = { '$ne': true };
+  callback(null, this);
+};
+
+baseSchema.statics.getAll = function(options, callback){
+  var params = options.hasOwnProperty('params') ? options.params : {};
+  params.isDeleted = { '$ne': true };
+  this.find(params, callback);
+};
+
 baseSchema.methods.findAndModify = function (query, sort, doc, options, callback) {
   return this.collection.findAndModify(query, sort, doc, options, callback);
 };
 
-module.exports.BaseSchema = baseSchema;
+module.exports.BaseModel = mongoose.model('BaseModel', baseSchema);
+

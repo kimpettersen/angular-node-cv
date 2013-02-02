ecbbd2742d388a16c266a8fde9b18fb7380573d9
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-02-01 23:34:31 -0500

    Started tests education

---
+++ server/tests/educationTests.js
@@ -0,0 +1,25 @@
+var should = require('should');
+
+describe('Education', function(){
+  describe('Post education', function(done){
+
+  });
+  describe('Get all education', function(done){
+    it('Should return 200 OK', function()err, res{
+      should.not.exist(err);
+      res.statusCode.should.be.equal(200);
+      done();
+    });
+  });
+  describe('Get one education', function(done){
+    it()
+  });
+
+  describe('Put education', function(done){
+
+  });
+  describe('Delete all education', function(done){
+
+  });
+
+});

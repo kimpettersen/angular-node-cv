f7517e8f07d8f1573e5c4db20cf621f28a0da71c
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-02-07 13:58:54 -0500

    Debugging ng-include

--- app/scripts/controllers/adminCtrl.js
+++ app/scripts/controllers/adminCtrl.js
@@ -3,15 +3,15 @@
 
 CVApp.controller('AdminCtrl', function($scope){
   $scope.templates =
-    [ { name: 'Bucketlist', url: '/admin/bucketlist.html'},
-      { name: 'Education', url: '/admin/education.html'},
-      { name: 'Experience', url: '/admin/experience.html'}, 
-      { name: 'Me', url: '/admin/me.html'},
-      { name: 'User', url: '/admin/user.html'} ];
+    [ { name: 'Bucketlist', url: '/#/admin/bucketlist.html'},
+      { name: 'Education', url: '/#/admin/education.html'},
+      { name: 'Experience', url: '/#/admin/experience.html'}, 
+      { name: 'Me', url: 'me.html'},
+      { name: 'User', url: 'user.html'} ];
   $scope.template = $scope.templates[0];
 
-  $scope.switchTemplate = function(index){
-    console.log('here')
+  $scope.changeTemplate = function(index){
+    console.log('sdfsdf')
     $scope.template = $scope.templates[index];
   };
 });

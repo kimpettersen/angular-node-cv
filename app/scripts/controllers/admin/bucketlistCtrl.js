e6c60d65f3c45b6f8e77d22872b3ef8671fcda1c
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-02-18 22:28:56 -0500

    added markdown js

--- app/scripts/controllers/admin/bucketlistCtrl.js
+++ app/scripts/controllers/admin/bucketlistCtrl.js
@@ -7,6 +7,33 @@ CVApp.controller('BucketlistCtrl', function( $scope, adminService ) {
   $scope.adminService.updateResources('bucketlist');
   $scope.currentItem = {};
   $scope.status = '';
+  var editorOpts = {
+    container: 'bucketlist-editor',
+    basePath: '/scripts/vendor/epiceditor-0.2.0/',
+    clientSideStorage: true,
+    localStorageName: 'bucketlist-editor',
+    useNativeFullsreen: true,
+    parser: marked,
+    file: {
+      name: 'bucketlist-editor',
+      defaultContent: '',
+      autoSave: 100
+    },
+    theme: {
+      base:'themes/base/epiceditor.css',
+      preview:'themes/preview/preview-dark.css',
+      editor:'themes/editor/epic-dark.css'
+    },
+    focusOnLoad: false,
+    shortcut: {
+      modifier: 18,
+      fullscreen: 70,
+      preview: 80
+    }
+  }
+
+  var ed = new EpicEditor(editorOpts).load();
+
 
   $scope.show = function(item){
     $scope.currentItem = adminService.findById(item);

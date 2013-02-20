98c55f883ac635e9e99c2d82e3a224f49bb39ced
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-02-19 22:21:48 -0500

    Added e2e config files

---
+++ e2e_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo ""
+echo "Starting Testacular Server (http://vojtajina.github.com/testacular)"
+echo "-------------------------------------------------------------------"
+
+node_modules/testacular/bin/testacular start testacular.e2e.conf.js

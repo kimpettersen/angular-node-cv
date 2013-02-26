4f42cc5b4fef907fd73c92e9b729c7531dd9fec7
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-02-25 22:39:09 -0500

    Updated menu when logged in. added new service loginStatus

--- server/api/baseController.js
+++ server/api/baseController.js
@@ -19,9 +19,12 @@ module.exports.resultHandler = function(error, result, response, returnCode, cal
 module.exports.protect = function(req, res, next) {
   if(!req.session.user_id) {
     res.status(403);
-    if (res.headers.CVAppAuth !== undefined){
-      delete res.headers.CVAppAuth;
+    if(res.headers){
+      if (res.headers.CVAppAuth){
+        delete res.headers.CVAppAuth;
+      }
     }
+
     res.json('You are not authorized to view this page');
   } else {
     res.setHeader('CVAppAuth', 'true');

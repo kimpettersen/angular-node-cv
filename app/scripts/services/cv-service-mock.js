30cccac79cb5b987c38ae96ffdc98fe3b07871f0
Kim Pettersen <kim.a.pettersen@gmail.com>
2013-03-05 10:36:45 -0500

    0

---
+++ app/scripts/services/cv-service-mock.js
@@ -0,0 +1,37 @@
+angular.module('CVServicesMock', [])
+  .factory('CVServiceMock', function(){
+
+  var adminService = function(type){
+    //set the right type for the current ng-model
+    //Example: adminService.user.item
+    this[type] = {};
+    this[type].item = type;
+  };
+
+  adminService.prototype.findById = function(item){
+    return item;
+  };
+
+  adminService.prototype.updateResources = function(item){
+    return item;
+  };
+
+  adminService.prototype.delItem = function(options, callback){
+    //pass the message you want back
+    return callback(options.message);
+  };
+
+  adminService.prototype.editResource = function(options, callback){
+    //pass the message you want back
+    return callback(options.message);
+  };
+
+  adminService.prototype.createResource = function(options, callback){
+    //pass the message you want back
+    return callback(options.message);
+  };
+
+  return {
+    adminService: adminService
+  };
+});

9cade67adc3f64e240e886d5dbddf81872d6c780
Kim Pettersen <kim.a.pettersen@gmail.com>
2014-05-29 22:56:28 -0700

    Added a few more tests

--- test/e2e-new/adminTest.js
+++ test/e2e-new/adminTest.js
@@ -4,7 +4,12 @@ describe('Given I am on the Admin page', function() {
       institution = 'a institution',
       degree = 'a degree',
       description = 'a description',
-      order = 1;
+      order = 1,
+      company = 'a company',
+      title = 'a title',
+      duration = 'from - to',
+      username = 'admin',
+      password = '1234';
 
   function login(username, password) {
     username = username ? username : 'admin';
@@ -28,6 +33,27 @@ describe('Given I am on the Admin page', function() {
     element(by.css('#education .create-button')).click();
   }
 
+  function createExperienceItem() {
+    element(by.model('adminService.experience.item.company')).sendKeys(company);
+    element(by.model('adminService.experience.item.duration')).sendKeys(duration);
+    element(by.model('adminService.experience.item.description')).sendKeys(description);
+    element(by.model('adminService.experience.item.order')).sendKeys(order);
+    element(by.css('#experience .create-button')).click();
+  }
+
+  function createMeItem() {
+    element(by.model('adminService.me.item.title')).sendKeys(title);
+    element(by.model('adminService.me.item.description')).sendKeys(description);
+    element(by.model('adminService.me.item.order')).sendKeys(order);
+    element(by.css('#me .create-button')).click();
+  }
+
+  function createUserItem() {
+    element(by.model('adminService.user.item.username')).sendKeys(username);
+    element(by.model('adminService.user.item.password')).sendKeys(password);
+    element(by.css('#user .create-button')).click();
+  }
+
   function removeItems(name) {
     $('#' + name + ' .resource-list .delete-item-button').click();
   }
@@ -44,15 +70,15 @@ describe('Given I am on the Admin page', function() {
   it('should NOT have the initial test data', function() {
     // Remove initial test data used to test the API
     $('#education .resource-list .delete-item-button').click();
+    $('#experience .resource-list .delete-item-button').click();
+    $('#me .resource-list .delete-item-button').click();
+
     expect(element.all(by.css('#education .resource-list > div')).count()).toBe(0);
+    expect(element.all(by.css('#experience .resource-list > div')).count()).toBe(0);
+    expect(element.all(by.css('#me .resource-list > div')).count()).toBe(0);
   });
 
   describe('When I create a new EDUCATION item', function() {
-    var institution = 'a institution',
-        degree = 'a degree',
-        description = 'a description',
-        order = 1;
-
     beforeEach(function(){
       createEducationItem();
     });
@@ -104,17 +130,18 @@ describe('Given I am on the Admin page', function() {
     });
 
     describe('When I change the fields', function() {
-      var institution = 'new institution',
-          degree = 'new degree',
-          description = 'new description',
-          order = 2;
+      var newInstitution = 'new institution',
+          newDegree = 'new degree',
+          newDescription = 'new description',
+          newOrder = 2;
 
       beforeEach(function(){
+        element(by.css('#education .clear-button')).click();
         element(by.css('#education .edit-button')).click();
-        element(by.model('adminService.education.item.institution')).sendKeys(institution);
-        element(by.model('adminService.education.item.degree')).sendKeys(degree);
-        element(by.model('adminService.education.item.description')).sendKeys(description);
-        element(by.model('adminService.education.item.order')).sendKeys(order);
+        element(by.model('adminService.education.item.institution')).sendKeys(newInstitution);
+        element(by.model('adminService.education.item.degree')).sendKeys(newDegree);
+        element(by.model('adminService.education.item.description')).sendKeys(newDescription);
+        element(by.model('adminService.education.item.order')).sendKeys(newOrder);
         // Do the edit
         element(by.css('#education .edit-button')).click();
         //Clear the form
@@ -147,9 +174,293 @@ describe('Given I am on the Admin page', function() {
       expect(element(by.model('adminService.education.item.description')).getAttribute('value')).toEqual('');
       expect(element(by.model('adminService.education.item.order')).getAttribute('value')).toEqual('');
     });
-
-
-
   });
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  describe('When I create a new EXPERIENCE item', function() {
+    beforeEach(function(){
+      createExperienceItem();
+    });
+
+    afterEach(function() {
+      removeItems('experience')
+    });
+
+    it('should add the item to the resource list', function() {
+      expect(element.all(by.css('#experience .resource-list > div')).count()).toBe(1);
+    });
+
+    it('should add a resource with the right fields', function() {
+      element(by.css('#experience .resource-list .resource-name')).getText().then(function(text){
+        expect(text).toContain(company);
+      });
+    });
+  });
+
+  describe('When I want to DELETE an experience item', function() {
+
+    beforeEach(function(){
+      createExperienceItem();
+    });
+
+    it('should delete an item', function() {
+      removeItems('experience');
+      expect(element.all(by.css('#experience .resource-list > div')).count()).toBe(0);
+    });
+  });
+
+  describe('When I want to EDIT an item', function() {
+    beforeEach(function() {
+      createExperienceItem();
+      element(by.css('#experience .resource-list .show-item-button')).click();
+    });
+
+    afterEach(function() {
+      removeItems('experience');
+    });
+
+    describe('When I select an item to edit', function() {
+      it('should fill in the form', function() {
+        expect(element(by.model('adminService.experience.item.company')).getAttribute('value')).toContain(company);
+        expect(element(by.model('adminService.experience.item.duration')).getAttribute('value')).toContain(duration);
+        expect(element(by.model('adminService.experience.item.description')).getAttribute('value')).toContain(description);
+        expect(element(by.model('adminService.experience.item.order')).getAttribute('value')).toContain(order);
+      });
+    });
+
+    describe('When I change the fields', function() {
+      var newCompany = 'new company',
+          newDuration = 'new duration',
+          newDescription = 'new description',
+          newOrder = 2;
+
+      beforeEach(function(){
+        element(by.css('#experience .clear-button')).click();
+        element(by.css('#experience .edit-button')).click();
+        element(by.model('adminService.experience.item.company')).sendKeys(newCompany);
+        element(by.model('adminService.experience.item.duration')).sendKeys(newDuration);
+        element(by.model('adminService.experience.item.description')).sendKeys(newDescription);
+        element(by.model('adminService.experience.item.order')).sendKeys(newOrder);
+        // Do the edit
+        element(by.css('#experience .edit-button')).click();
+        //Clear the form
+        element(by.css('#experience .clear-button')).click();
+        //Display it again
+        element(by.css('#experience .resource-list .show-item-button')).click();
+
+      });
+
+      it('should have changed the entry' , function() {
+        expect(element(by.model('adminService.experience.item.company')).getAttribute('value')).toContain(company);
+        expect(element(by.model('adminService.experience.item.duration')).getAttribute('value')).toContain(duration);
+        expect(element(by.model('adminService.experience.item.description')).getAttribute('value')).toContain(description);
+        expect(element(by.model('adminService.experience.item.order')).getAttribute('value')).toContain(order);
+      });
+
+    });
+  });
+
+  describe('When I want to clear the form', function() {
+    beforeEach(function(){
+      createExperienceItem();
+      element(by.css('#experience .resource-list .show-item-button')).click();
+      element(by.css('#experience .clear-button')).click();
+    });
+
+    it('should have an empty form', function() {
+      expect(element(by.model('adminService.experience.item.company')).getAttribute('value')).toEqual('');
+      expect(element(by.model('adminService.experience.item.duration')).getAttribute('value')).toEqual('');
+      expect(element(by.model('adminService.experience.item.description')).getAttribute('value')).toEqual('');
+      expect(element(by.model('adminService.experience.item.order')).getAttribute('value')).toEqual('');
+    });
+  });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  describe('When I create a new ME item', function() {
+    beforeEach(function(){
+      createMeItem();
+    });
+
+    afterEach(function() {
+      removeItems('me')
+    });
+
+    it('should add the item to the resource list', function() {
+      expect(element.all(by.css('#me .resource-list > div')).count()).toBe(1);
+    });
+
+    it('should add a resource with the right fields', function() {
+      element(by.css('#me .resource-list .resource-name')).getText().then(function(text){
+        expect(text).toContain(title);
+      });
+    });
+  });
+
+  describe('When I want to DELETE an me item', function() {
+
+    beforeEach(function(){
+      createMeItem();
+    });
+
+    it('should delete an item', function() {
+      removeItems('me');
+      expect(element.all(by.css('#me .resource-list > div')).count()).toBe(0);
+    });
+  });
+
+  describe('When I want to EDIT an item', function() {
+    beforeEach(function() {
+      createMeItem();
+      element(by.css('#me .resource-list .show-item-button')).click();
+    });
+
+    afterEach(function() {
+      removeItems('me');
+    });
+
+    describe('When I select an item to edit', function() {
+      it('should fill in the form', function() {
+        expect(element(by.model('adminService.me.item.title')).getAttribute('value')).toContain(title);
+        expect(element(by.model('adminService.me.item.description')).getAttribute('value')).toContain(description);
+      });
+    });
+
+    describe('When I change the fields', function() {
+      var newCompany = 'new title',
+          newDuration = 'new duration',
+          newDescription = 'new description',
+          newOrder = 2;
+
+      beforeEach(function(){
+        element(by.css('#me .clear-button')).click();
+        element(by.css('#me .edit-button')).click();
+        element(by.model('adminService.me.item.title')).sendKeys(newCompany);
+        element(by.model('adminService.me.item.description')).sendKeys(newDescription);
+        // Do the edit
+        element(by.css('#me .edit-button')).click();
+        //Clear the form
+        element(by.css('#me .clear-button')).click();
+        //Display it again
+        element(by.css('#me .resource-list .show-item-button')).click();
+
+      });
+
+      it('should have changed the entry' , function() {
+        expect(element(by.model('adminService.me.item.title')).getAttribute('value')).toContain(title);
+        expect(element(by.model('adminService.me.item.description')).getAttribute('value')).toContain(description);
+      });
+
+    });
+  });
+
+  describe('When I want to clear the form', function() {
+    beforeEach(function(){
+      createMeItem();
+      element(by.css('#me .resource-list .show-item-button')).click();
+      element(by.css('#me .clear-button')).click();
+    });
+
+    it('should have an empty form', function() {
+      expect(element(by.model('adminService.me.item.title')).getAttribute('value')).toEqual('');
+      expect(element(by.model('adminService.me.item.description')).getAttribute('value')).toEqual('');
+    });
+  });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  // describe('When I create a new USER item', function() {
+  //   beforeEach(function(){
+  //     createUserItem();
+  //   });
+
+  //   afterEach(function() {
+  //     removeItems('user')
+  //   });
+
+  //   it('should add the item to the resource list', function() {
+  //     expect(element.all(by.css('#user .resource-list > div')).count()).toBe(1);
+  //   });
+
+  //   it('should add a resource with the right fields', function() {
+  //     element(by.css('#user .resource-list .resource-name')).getText().then(function(text){
+  //       expect(text).toContain(title);
+  //     });
+  //   });
+  // });
+
+  // describe('When I want to DELETE an me item', function() {
+
+  //   beforeEach(function(){
+  //     createUserItem();
+  //   });
+
+  //   it('should delete an item', function() {
+  //     removeItems('user');
+  //     expect(element.all(by.css('#user .resource-list > div')).count()).toBe(0);
+  //   });
+  // });
+
+  // describe('When I want to EDIT an item', function() {
+  //   beforeEach(function() {
+  //     createUserItem();
+  //     element(by.css('#user .resource-list .show-item-button')).click();
+  //   });
+
+  //   afterEach(function() {
+  //     removeItems('user');
+  //   });
+
+  //   describe('When I select an item to edit', function() {
+  //     it('should fill in the form', function() {
+  //       expect(element(by.model('adminService.user.item.username')).getAttribute('value')).toContain(username);
+  //       expect(element(by.model('adminService.user.item.password')).getAttribute('value')).toContain(password);
+  //     });
+  //   });
+
+  //   describe('When I change the fields', function() {
+  //     var newUsername = 'new username',
+  //         newPassword = 'new password';
+
+  //     beforeEach(function(){
+  //       element(by.css('#user .clear-button')).click();
+  //       element(by.css('#user .edit-button')).click();
+  //       element(by.model('adminService.user.item.username')).sendKeys(newUsername);
+  //       element(by.model('adminService.user.item.password')).sendKeys(newPassword);
+  //       // Do the edit
+  //       element(by.css('#user .edit-button')).click();
+  //       //Clear the form
+  //       element(by.css('#user .clear-button')).click();
+  //       //Display it again
+  //       element(by.css('#user .resource-list .show-item-button')).click();
+
+  //     });
+
+  //     it('should have changed the entry' , function() {
+  //       expect(element(by.model('adminService.user.item.username')).getAttribute('value')).toContain(username);
+  //       expect(element(by.model('adminService.user.item.password')).getAttribute('value')).toContain(password);
+  //     });
+
+  //   });
+  // });
+
+  // describe('When I want to clear the form', function() {
+  //   beforeEach(function(){
+  //     createUserItem();
+  //     element(by.css('#user .resource-list .show-item-button')).click();
+  //     element(by.css('#user .clear-button')).click();
+  //   });
+
+  //   it('should have an empty form', function() {
+  //     expect(element(by.model('adminService.user.item.username')).getAttribute('value')).username('');
+  //     expect(element(by.model('adminService.user.item.password')).getAttribute('value')).password('');
+  //   });
+  // });
+
 
 });
